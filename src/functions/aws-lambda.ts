5076f549d410ec815f487e28a168ce9acc30152a
Q <qesserahmad@gmail.com>
2023-08-17 02:57:43 +0100

    added basic aws based lambda boilerplate

---
+++ src/functions/aws-lambda.ts
@@ -0,0 +1,13 @@
+// import {} from "/src/types"
+// import {} from "@aws-sdk/client-{service}"
+
+// const awsServiceClient = new ServiceClient()
+
+export const handler = async (event: any) => {
+    try {
+        
+    } catch (err) {
+        console.error(err)
+        throw err
+    }
+}
