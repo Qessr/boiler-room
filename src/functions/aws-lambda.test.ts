44e6a495f3d0021bc4057c5e747c5708ad22b897
Qesser Ahmad <qesser@Qessers-MacBook-Pro.local>
2023-08-25 16:24:20 +0100

    added test boilerplate

---
+++ src/functions/aws-lambda.test.ts
@@ -0,0 +1,14 @@
+import { handler } from './aws-lambda';
+import { EventBridgeEvent } from 'aws-lambda';
+
+describe('aws-lambda', () => {
+    beforeEach(() => {
+        jest.clearAllMocks()
+    })
+
+    describe('given x', () => {
+        it ('should y', async () => {
+
+        })
+    })
+})
